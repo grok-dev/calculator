17de78043ad3cce3effe0d815fe9e6910e06b060
grok-dev <cav3m4nuo@gmail.com>
2022-10-01 21:19:24 -0700

    add number population

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function divide(x, y) {
   return x / y;
 }
 
-function operate(operator, x, y) {
+function operate(x, operator, y) {
   x = Number(x);
   y = Number(y);
 
@@ -23,3 +23,49 @@ function operate(operator, x, y) {
   if (operator === "*") return multiply(x, y);
   if (operator === "/") return divide(x, y);
 }
+
+const display = document.querySelector(".display");
+let displayValue = "";
+let firstNum = "";
+let operation = "";
+
+function updateDisplay(n = "") {
+  displayValue += n;
+  display.innerHTML = displayValue;
+}
+
+function resetDisplay() {
+  displayValue = "0";
+  updateDisplay();
+}
+
+document.getElementById("1").onclick = () => {
+  updateDisplay(1);
+};
+document.getElementById("2").onclick = () => {
+  updateDisplay(2);
+};
+document.getElementById("3").onclick = () => {
+  updateDisplay(3);
+};
+document.getElementById("4").onclick = () => {
+  updateDisplay(4);
+};
+document.getElementById("5").onclick = () => {
+  updateDisplay(5);
+};
+document.getElementById("6").onclick = () => {
+  updateDisplay(6);
+};
+document.getElementById("7").onclick = () => {
+  updateDisplay(7);
+};
+document.getElementById("8").onclick = () => {
+  updateDisplay(8);
+};
+document.getElementById("9").onclick = () => {
+  updateDisplay(9);
+};
+document.getElementById("0").onclick = () => {
+  updateDisplay(0);
+};
