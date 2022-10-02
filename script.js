b6a5adc2bf4e7e9eed3253fb04d2ab1dab03f147
grok-dev <cav3m4nuo@gmail.com>
2022-10-01 18:13:00 -0700

    Add Math Functions

---
+++ script.js
@@ -0,0 +1,25 @@
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  return x / y;
+}
+
+function operate(operator, x, y) {
+  x = Number(x);
+  y = Number(y);
+
+  if (operator === "+") return add(x, y);
+  if (operator === "-") return subtract(x, y);
+  if (operator === "*") return multiply(x, y);
+  if (operator === "/") return divide(x, y);
+}
